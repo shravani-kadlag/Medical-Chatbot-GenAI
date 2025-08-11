e0c27f9d70c1082ed61af1b8f536fd483ac61a05
shravani-kadlag <kadlagshravani2004@gmail.com>
2025-08-11 20:38:25 +0530

    folder added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name= 'Generative AI Project' ,
+    version= '0.0.0',
+    author='Shravani kadlag',
+    author_email='kadlagshravaani2004@gmail.com',
+    packages= find_packages(),
+    install_requires =[]
+)
---
+++ store_index.py
@@ -0,0 +1,52 @@
+from src.helper import load_pdf_file,text_split , download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as PineconeGRPC
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv("C:\\Users\\SHANTANU KADLAG\\OneDrive\\Desktop\\medical bot\\Medical-Chatbot-GenAI\\.env")
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+
+extracted_data=load_pdf_file(data=r"C:\Users\SHANTANU KADLAG\OneDrive\Desktop\medical bot\Medical-Chatbot-GenAI\Data")
+text_chunks=text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+
+
+pc = Pinecone(api_key= PINECONE_API_KEY)
+
+index_name = "medicalbot"
+
+pc.create_index(
+    name=index_name,
+    dimension=384,
+    metric="cosine",
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+
+
+
+
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings, 
+)
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
